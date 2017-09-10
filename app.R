eba1b040791bc1029a15676ad352c78f73b2787f
Demolud <iwo_a@o2.pl>
2017-09-10 21:58:33 +0200

    Nazwy krajów po polsku

--- app.R
+++ app.R
@@ -20,11 +20,12 @@ ui <- fluidPage(theme = "bootstrap.css",
      fluidRow( column (1), column (11,
                                    selectInput("bins",
                                                "   Wybór kraju",
-                                               choices = list("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EA","EA12",
-                                                              "EA19", "EE", "EL", "ES", "EU15", "EU28", "FI", "FR",
-                                                              "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MK", "NL",
-                                                              "NO", "PL", "PT", "RO", "RS", "SE", "SI", "SK", "UK"),
-                                               selected = "EU28")
+                                               choices = list("Austria", "Belgia", "Bułgaria", "Chorwacja", "Cypr", "Czechy", "Dania", "Estonia",
+                                                              "Finlandia", "Francja", "Grecja", "Hiszpania", "Holandia", "Irlandia",
+                                                              "Litwa", "Luksemburg", "Łotwa", "Malta", "Niemcy", "Polska", "Portugalia", "Rumunia", 
+                                                              "Szwecja", "Słowenia", "Słowacja", "Węgry", "Wlk. Brytania", "Włochy",
+                                                              "Strefa Euro",  "UE 15", "UE 28"),
+                                               selected = "UE 28")
      )),
      
      # Show a plot of the generated distribution
@@ -43,6 +44,18 @@ library(tidyr)
 library(ggplot2)
 library(ggthemes)
 
+countries <- cbind(c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EA",
+                "EE", "EL", "ES", "EU15", "EU28", "FI", "FR",
+                "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MK", "NL",
+                "PL", "PT", "RO", "SE", "SI", "SK", "UK"),
+                c("Austria", "Belgia", "Bułgaria", "Cypr", "Czechy", "Niemcy",
+                  "Dania", "Strefa Euro", "Estonia", "Grecja", "Hiszpania", "UE 15",
+                  "UE 28", "Finlandia", "Francja", "Chorwacja", "Węgry", "Irlandia",
+                  "Włochy", "Litwa", "Luksemburg", "Łotwa", "Malta", "Holandia", "Polska",
+                  "Portugalia", "Rumunia", "Szwecja", "Słowenia", "Słowacja", "Wlk. Brytania")
+                    )
+colnames(countries) <- c("geo", "geo1")
+
 gdp<-get_eurostat("nama_10_gdp") %>% 
      filter(time >= "2002-01-01") %>%
      filter(time <= "2016-01-01") %>%
@@ -50,7 +63,8 @@ gdp<-get_eurostat("nama_10_gdp") %>%
      subset(.,na_item %in% c("P3_S13", "P31_S14_S15", "P51G", "P52_P53",
                              "P6", "P7", "D1", "B2A3G", "D2X3")) %>%
      select(geo, time, na_item, values)
-
+gdp<-right_join(gdp, as.data.frame(countries))
+gdp$geo<-as.factor(gdp$geo)
 #SP=D1+B2A3G
 #I=P51G+P52_P53
 #G=P3_S13
@@ -68,13 +82,13 @@ tmp<-spread(gdp,na_item, values) %>%
      mutate (., dE=P6-P7) %>%
      mutate (., ver=dSP-(dG+dE))
 
-res<-select(tmp, geo, time, dSP, dG, dE, ver)
-# Define server logic required to draw a histogram
+res<-select(tmp, geo1, time, dSP, dG, dE, ver)
+# Define server logic required to draw a graph
 server <- function(input, output) {
      
      output$distPlot <- renderPlot({
           # generate bins based on input$bins from ui.R
-          x<-filter(res, geo==input$bins)
+          x<-filter(res, geo1==input$bins)
           x$dG<--x$dG
           x$dE<--x$dE
           x1<-gather(x, na_item, values, 3:5)
@@ -86,7 +100,7 @@ server <- function(input, output) {
                theme_hc() + scale_fill_brewer(palette="Set2") +
                scale_y_continuous(labels=function(x)x/1000) +
                labs(fill = "Bilans sektora: ") + ylab("mld") + xlab("rok") +
-               ggtitle (levels(x1$geo)[as.numeric(as.character(x1[1,1]))]) +
+               ggtitle (levels(x1$geo1)[as.numeric(as.character(x1[1,1]))]) +
                theme(plot.background = element_rect(fill = "#fcfcfc"),
                      legend.background = element_rect(fill = "#fcfcfc"))
      })
